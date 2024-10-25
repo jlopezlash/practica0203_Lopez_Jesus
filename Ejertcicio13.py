62c31dc827a9429d993aa2065cfaac958099c77a
jlopezlash <jlopezlash1@educacion.navarra.es>
2024-10-25 13:20:05 +0200

    Create Ejertcicio13.py

---
+++ Ejertcicio13.py
@@ -0,0 +1,5 @@
+#Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta que el usuario escriba “salir” que terminará.
+while True:
+    Palabra = input('Escribe algo ')
+    if Palabra == 'salir':
+        break
