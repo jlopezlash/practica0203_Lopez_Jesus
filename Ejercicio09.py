3f2a2191faa9fd2d6c1a1f6cf49b0e99bcce09f0
jlopezlash <jlopezlash1@educacion.navarra.es>
2024-10-25 13:20:29 +0200

    Update Ejercicio09.py

--- Ejercicio09.py
+++ Ejercicio09.py
@@ -1,10 +1,12 @@
 '''Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo
-#que tenga tantas líneas como el número introducido, como el triángulo de más abajo
+que tenga tantas líneas como el número introducido, como el triángulo de más abajo
 1
 3 1
 5 3 1
 7 5 3 1
 '''
-Altura = int(input('Dime un número entero '))
-for i in range(1, Altura + 1, 2):
-    print(1 * i)
+x = int(input('Dime un número entero '))
+for i in range(1, x + 1, 2):
+    for b in range(i, 0, -2):
+        print(b, end=' ')
+    print('')
