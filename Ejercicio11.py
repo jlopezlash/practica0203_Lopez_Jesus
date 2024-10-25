713f96ce41edc9b7e336c43cba4a8e76523ecd25
jlopezlash <jlopezlash1@educacion.navarra.es>
2024-10-25 13:20:26 +0200

    Update Ejercicio11.py

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,5 +1,5 @@
 #Escribir un programa que pida al usuario una palabra y luego muestre por pantalla una a una las letras
 #de la palabra introducida empezando por la última.
 x = input('Dime una palabra ')
-for i in range(len(x) - 1, -1, -1):
-    print(x[i])
+for i in range(len(x) - 1, -1, -1):     #Se pone len para sacar el numero de caracteres
+    print(x[i])                         # y se le añade un -1.
